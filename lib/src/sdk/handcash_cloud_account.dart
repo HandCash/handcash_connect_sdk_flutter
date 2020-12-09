b397a6de993ca7ae45c404c8515275cba0a5da1a
Alfredo Bautista <alfredo@MacBook-Pro-de-Alfredo.local>
2020-12-09 12:55:21 +0100

    rename HandCashCloudAccount

---
+++ lib/src/sdk/handcash_cloud_account.dart
@@ -0,0 +1,29 @@
+import 'package:handcash_connect_sdk/src/environments.dart';
+import 'package:handcash_connect_sdk/src/sdk/profile.dart';
+import 'package:handcash_connect_sdk/src/sdk/wallet.dart';
+import 'package:handcash_connect_sdk/src/services/service_factory.dart';
+
+class HandCashCloudAccount {
+  final Wallet wallet;
+  final Profile profile;
+
+  HandCashCloudAccount({
+    this.wallet,
+    this.profile,
+  });
+
+  factory HandCashCloudAccount.fromAuthToken({
+    String authToken,
+    Environment environment = const Environment.production(),
+  }) {
+    final serviceFactory = ServiceFactory(
+      authToken: authToken,
+      baseApiEndpoint: environment.apiEndpoint,
+    );
+
+    return HandCashCloudAccount(
+      wallet: Wallet(serviceFactory.getWalletService()),
+      profile: Profile(serviceFactory.getProfileService()),
+    );
+  }
+}
