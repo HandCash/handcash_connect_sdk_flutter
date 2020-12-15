ddeee2f69b7b4039d092c77aa171de5a70833e60
Rafa JS <rjseibane@gmail.com>
2020-12-16 00:40:38 +0100

    Refactor auth token listener

--- lib/handcash_connect_sdk.dart
+++ lib/handcash_connect_sdk.dart
@@ -1,11 +1,11 @@
 library handcash_connect_sdk;
 
+export './src/environments.dart';
 export './src/link/handcash_auth_token_listener.dart';
 export './src/models/models.dart';
-export './src/sdk/handcash_connect.dart';
 export './src/sdk/handcash_cloud_account.dart';
-export './src/environments.dart';
-export './src/sdk/wallet.dart';
+export './src/sdk/handcash_connect.dart';
 export './src/sdk/profile.dart';
+export './src/sdk/wallet.dart';
 export './src/services/handcash_connect_api_error.dart';
 export './src/ui/connect_button.dart';
